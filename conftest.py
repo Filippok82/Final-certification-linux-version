86d9fe187043cefb4a5c92cde6dad143b58bc6c3
Filippok82 <irina.filippova.mail@yandex.ru>
2023-06-07 15:05:15 +0300

    New project final work

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 import yaml
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
